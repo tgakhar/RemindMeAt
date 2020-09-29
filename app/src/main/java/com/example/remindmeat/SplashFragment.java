3bb50fb5bd6a64575fd8d2da6f6ae75269d7ff3c
Chintan22 <Dankharachintan@gmail.com>
2020-09-28 20:06:55 -0400

    Animation added for splash screen.

--- app/src/main/java/com/example/remindmeat/SplashFragment.java
+++ app/src/main/java/com/example/remindmeat/SplashFragment.java
@@ -2,57 +2,47 @@ package com.example.remindmeat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SplashFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SplashFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    Animation topAnim,bottomAnim;
+    TextView txt_title,txt_tag;
+    ImageView img_main;
     public SplashFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SplashFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SplashFragment newInstance(String param1, String param2) {
-        SplashFragment fragment = new SplashFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        topAnim= AnimationUtils.loadAnimation(getActivity(),R.anim.top_animation);
+        bottomAnim=AnimationUtils.loadAnimation(getActivity(),R.anim.bottom_animation);
+        txt_title=view.findViewById(R.id.txt_title);
+        img_main=view.findViewById(R.id.img_splashMain);
+        txt_tag=view.findViewById(R.id.txt_tag);
+        img_main.setAnimation(topAnim);
+        txt_title.setAnimation(bottomAnim);
+        txt_tag.setAnimation(bottomAnim);
     }
 
     @Override
