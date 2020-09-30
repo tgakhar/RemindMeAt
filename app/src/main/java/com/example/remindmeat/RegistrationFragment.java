915fcd6ea2ea0d3ca93a0adcc8006756d0d736b3
sarbjit kaur <nishuuukaur12@gmail.com>
2020-09-30 16:23:44 -0400

    Registration using firebase auth done.

--- app/src/main/java/com/example/remindmeat/RegistrationFragment.java
+++ app/src/main/java/com/example/remindmeat/RegistrationFragment.java
@@ -2,58 +2,107 @@ package com.example.remindmeat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegistrationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class RegistrationFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    TextView txt_login;
+    Button btn_register;
+    TextInputLayout txt_layEmail,txt_layPass,txt_layName,txt_layCPass;
+    FirebaseAuth auth;
+    FirebaseFirestore db;
+    String name,email,pass,cPass;
     public RegistrationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrationFragment newInstance(String param1, String param2) {
-        RegistrationFragment fragment = new RegistrationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        auth=FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
+        txt_login=view.findViewById(R.id.txt_registerLogin);
+        txt_layEmail=view.findViewById(R.id.edt_registerEmail);
+        txt_layPass=view.findViewById(R.id.edt_registerPass);
+        txt_layCPass=view.findViewById(R.id.edt_registerCPass);
+        txt_layName=view.findViewById(R.id.edt_registerName);
+        btn_register=view.findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(registerUser);
+    }
+
+
+
+    View.OnClickListener registerUser=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            name=txt_layName.getEditText().getText().toString().trim();
+            email=txt_layEmail.getEditText().getText().toString().trim();
+            pass=txt_layPass.getEditText().getText().toString().trim();
+            cPass=txt_layCPass.getEditText().getText().toString().trim();
+
+
+
+            final Map<String,Object> usermap=new HashMap<>();
+            usermap.put("Name",name);
+            usermap.put("Email",email);
+
+            if (!checkEmptyField()){
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            FirebaseUser user = auth.getCurrentUser();
+                            db.collection("Users").document(user.getUid()).set(usermap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(getActivity().getApplicationContext(), "Successfully Registered!", Toast.LENGTH_LONG).show();
+                                    NavController navController= Navigation.findNavController(getActivity(),R.id.nav_host_main);
+                                    navController.navigate(R.id.loginFragment);
+                                }
+                            });
+                        }
+                    }
+                });
+            }else{
+                return;
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,4 +110,47 @@ public class RegistrationFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registration, container, false);
     }
+
+    public boolean checkEmptyField() {
+
+        if (TextUtils.isEmpty(name)) {
+            txt_layName.setError("Name Cannot be Empty!");
+            txt_layName.requestFocus();
+            return true;
+        }else if (TextUtils.isEmpty(email)) {
+            txt_layName.setError(null);
+            txt_layName.setErrorEnabled(false);
+            txt_layEmail.setError("Email Cannot be Empty!");
+            txt_layEmail.requestFocus();
+            return true;
+        } else if (TextUtils.isEmpty(pass)) {
+            txt_layEmail.setError(null);
+            txt_layEmail.setErrorEnabled(false);
+            txt_layPass.setError("Password Cannot be Empty!");
+            txt_layPass.requestFocus();
+            return true;
+        }else if (TextUtils.isEmpty(cPass)){
+            txt_layPass.setError(null);
+            txt_layPass.setErrorEnabled(false);
+            txt_layCPass.setError("Confirm Password Cannot be Empty!");
+            txt_layCPass.requestFocus();
+            return true;
+        }else if (pass.length()<6){
+            txt_layPass.getEditText().getText().clear();
+            txt_layPass.setError("Password Cannot less than 6 characters");
+            txt_layPass.requestFocus();
+            return true;
+        }else if (!pass.equals(cPass)){
+            txt_layPass.getEditText().getText().clear();
+            txt_layCPass.getEditText().getText().clear();
+            txt_layPass.setError("Password not matched");
+            txt_layPass.requestFocus();
+            return true;
+        }
+        txt_layPass.setErrorEnabled(false);
+        txt_layCPass.setErrorEnabled(false);
+        return false;
+    }
+
+
 }
