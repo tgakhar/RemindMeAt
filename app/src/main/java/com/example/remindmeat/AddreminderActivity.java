2e746c008296b5da9649545c08f2efc397a82382
Chintan22 <Dankharachintan@gmail.com>
2020-10-05 11:01:59 -0400

    Layout of adding reminder created.

---
+++ app/src/main/java/com/example/remindmeat/AddreminderActivity.java
@@ -0,0 +1,14 @@
+package com.example.remindmeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddreminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addreminder);
+    }
+}
