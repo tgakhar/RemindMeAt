597c96099ccfa99693d6e862bbbeb2b79c822002
akshay <akshay9varma@gmail.com>
2020-10-05 22:53:07 -0400

    Fore AddreminderActivity showing slider value in textview and material date picker added.

--- app/src/main/java/com/example/remindmeat/AddreminderActivity.java
+++ app/src/main/java/com/example/remindmeat/AddreminderActivity.java
@@ -1,5 +1,6 @@
 package com.example.remindmeat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,8 +8,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,22 +20,54 @@ import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.DateValidatorPointForward;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.slider.Slider;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class AddreminderActivity extends AppCompatActivity {
     AutocompleteSupportFragment autocompleteFragmentLocation;
     LatLng latLng;
-    String address;
+    Slider slider;
+    TextView txt_rangeValue;
+    SwitchMaterial repeatSwitch;
+    TextInputLayout edt_date,edt_title,edt_description;
+    Button btn_addReminder;
+    String title,address,description,date;
+    int repeatMode,range;
+    int status = 1;
+    FirebaseAuth auth;
+    FirebaseFirestore db;
+    FirebaseUser curUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addreminder);
 
-
-
+        auth=FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
+        slider=findViewById(R.id.slider);
+        txt_rangeValue=findViewById(R.id.txt_addRangeSelected);
+        edt_date=findViewById(R.id.edt_addDate);
+        edt_title=findViewById(R.id.edt_addTitle);
+        edt_description=findViewById(R.id.edt_addDescription);
+        repeatSwitch=findViewById(R.id.switch_addRepeat);
+        btn_addReminder=findViewById(R.id.btn_addReminder);
+        edt_date.getEditText().setOnClickListener(datePicker);
+        slider.addOnChangeListener(updateSlider);
         autocompleteFragmentLocation= (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.location_fragment);
         autoCompleteFragment();
@@ -61,6 +96,43 @@ public class AddreminderActivity extends AppCompatActivity {
         });
 
     }
+    Slider.OnChangeListener updateSlider=new Slider.OnChangeListener() {
+        @Override
+        public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+            txt_rangeValue.setText(((int) value)+" m");
+
+        }
+    };
+
+    View.OnClickListener datePicker=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Calendar calender = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            calender.clear();
+
+            Long today = MaterialDatePicker.todayInUtcMilliseconds();
+
+            calender.setTimeInMillis(today);
+
+            CalendarConstraints.Builder constraint = new CalendarConstraints.Builder();
+            constraint.setValidator(DateValidatorPointForward.now());
+
+            MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+            builder.setTitleText("Please select date");
+            builder.setCalendarConstraints(constraint.build());
+            final MaterialDatePicker<Long> materialDatePicker = builder.build();
+            builder.setSelection(today);
+            materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+
+            materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                @Override
+                public void onPositiveButtonClick(Object selection) {
+                    edt_date.getEditText().setText(materialDatePicker.getHeaderText());
+
+                }
+            });
+        }
+    };
 
 
     private void setSearchUI() {
