db27b17ba4ca82356a57715d2db1db6e44175f18
Dhruv <dhruvj5418@gmail.com>
2020-10-12 12:59:06 -0400

    layout created for showing Reminder details.

---
+++ app/src/main/java/com/example/remindmeat/ReminderdetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.remindmeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReminderdetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminderdetails);
+    }
+}
