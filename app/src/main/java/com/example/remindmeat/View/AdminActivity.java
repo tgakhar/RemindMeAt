92fb0ab16b9fa790321ebf998347d4a8ae0931da
Chintan22 <Dankharachintan@gmail.com>
2020-10-12 13:24:22 -0400

    In AdminActivity toolbar and recyclerView element added with signout java code.

--- app/src/main/java/com/example/remindmeat/View/AdminActivity.java
+++ app/src/main/java/com/example/remindmeat/View/AdminActivity.java
@@ -2,20 +2,34 @@ package com.example.remindmeat.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.remindmeat.R;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
+    MaterialToolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         auth=FirebaseAuth.getInstance();
+        toolbar=findViewById(R.id.topAdminAppBar);
 
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+                Intent intent=new Intent(AdminActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
