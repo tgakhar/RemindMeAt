52b84a1f5a4c7feb1d3ab07a737d2f03f3af5e2c
Tanvi <gakhartanvi@gmail.com>
2020-10-13 13:27:14 -0400

    expandable fab menu created in ListFragment for filter.

--- app/src/main/java/com/example/remindmeat/View/ListFragment.java
+++ app/src/main/java/com/example/remindmeat/View/ListFragment.java
@@ -1,6 +1,7 @@
 package com.example.remindmeat.View;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -14,7 +15,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.remindmeat.Adapter.ReminderAdapter;
@@ -24,6 +28,7 @@ import com.example.remindmeat.ReminderdetailsActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -46,6 +51,12 @@ public class ListFragment extends Fragment {
     RecyclerView recyclerView_listReminder;
     ReminderAdapter reminderAdapter;
 
+    //For exapndable Floating menu
+    private FloatingActionButton fab_main, fab_all, fab_inactive,fab_active;
+    private Animation fab_open, fab_close, fab_clock, fab_anticlock;
+    TextView textview_all,textview_mail, textview_active;
+
+    Boolean isOpen = false;
     public ListFragment() {
         // Required empty public constructor
     }
@@ -70,8 +81,87 @@ public class ListFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         recyclerView_listReminder = view.findViewById(R.id.recycler_listReminder);
         loadData();
+
+        fab_main = view.findViewById(R.id.fab);
+        fab_all = view.findViewById(R.id.fab_all);
+        fab_inactive = view.findViewById(R.id.fab_inactive);
+        fab_active=view.findViewById(R.id.fab_active);
+        fab_close = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_close);
+        fab_open = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_open);
+        fab_clock = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_rotate_clock);
+        fab_anticlock = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_rotate_anticlock);
+
+        textview_mail = (TextView) view.findViewById(R.id.textview_inactive);
+        textview_active = (TextView) view.findViewById(R.id.textview_active);
+        textview_all=(TextView) view.findViewById(R.id.textview_all) ;
+
+        fab_main.setOnClickListener(fabMain);
+        fab_active.setOnClickListener(fabActive);
+        fab_inactive.setOnClickListener(fabInactive);
+        fab_all.setOnClickListener(fabAll);
     }
 
+    View.OnClickListener fabMain=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (isOpen) {
+
+                textview_mail.setVisibility(View.INVISIBLE);
+                textview_active.setVisibility(View.INVISIBLE);
+                textview_all.setVisibility(View.INVISIBLE);
+                fab_inactive.startAnimation(fab_close);
+                fab_all.startAnimation(fab_close);
+                fab_active.startAnimation(fab_close);
+                fab_main.startAnimation(fab_anticlock);
+                fab_inactive.setClickable(false);
+                fab_all.setClickable(false);
+                fab_active.setClickable(false);
+                isOpen = false;
+            } else {
+                textview_mail.setVisibility(View.VISIBLE);
+                textview_active.setVisibility(View.VISIBLE);
+                textview_all.setVisibility(View.VISIBLE);
+                fab_inactive.startAnimation(fab_open);
+                fab_all.startAnimation(fab_open);
+                fab_active.startAnimation(fab_open);
+                fab_main.startAnimation(fab_clock);
+                fab_inactive.setClickable(true);
+                fab_all.setClickable(true);
+                fab_active.setClickable(true);
+                isOpen = true;
+            }
+        }
+    };
+
+
+    View.OnClickListener fabActive=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            fab_active.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.Gray)));
+            fab_inactive.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.White)));
+            fab_all.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.White)));
+        }
+    };
+
+    View.OnClickListener fabInactive=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            fab_inactive.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.Gray)));
+            fab_active.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.White)));
+            fab_all.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.White)));
+        }
+    };
+
+    View.OnClickListener fabAll=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //fab_all.setBackground(getResources().getDrawable(R.drawable.clicked_floating));
+            fab_all.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.Gray)));
+            fab_inactive.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.White)));
+            fab_active.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.White)));
+            loadData();
+        }
+    };
 
     private void loadData() {
         curUser = auth.getCurrentUser();
