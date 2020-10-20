78a1bd5f3eb3d800a3cae745854697a81c8c94a2
karthik25111994 <karthik.bablu25@gmail.com>
2020-10-19 23:57:18 -0400

    In FeedbackActivity.java code added for email intent.

--- app/src/main/java/com/example/remindmeat/FeedbackActivity.java
+++ app/src/main/java/com/example/remindmeat/FeedbackActivity.java
@@ -2,13 +2,52 @@ package com.example.remindmeat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FeedbackActivity extends AppCompatActivity {
+    Button btn_send;
+    EditText txt_message;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        btn_send=findViewById(R.id.btn_feedBack);
+        txt_message=findViewById(R.id.edt_feedBack);
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message=txt_message.getText().toString();
+                sendFeedback();
+            }
+        });
+    }
+
+    private void sendFeedback() {
+        String[] TO = {"dhruvj5418@gmail.com","karthik.bablu25@gmail.com","gakhartanvi@gmail.com","nishuuukaur12@gmail.com","akshay9varma@gmail.com","dankharachintan@gmail.com"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for Remind Me At");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            //finish();
+            //Log.i("Finished sending email...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            //Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
