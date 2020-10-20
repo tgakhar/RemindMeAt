d3895d44a6dd666f420138f3dd081ad5cfbb0d1e
Chintan22 <Dankharachintan@gmail.com>
2020-10-20 12:08:08 -0400

    Volley call added to admin node.js API for deleting user from Firebase Authentication.

--- app/src/main/java/com/example/remindmeat/Adapter/AdminAdapter.java
+++ app/src/main/java/com/example/remindmeat/Adapter/AdminAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +20,8 @@ public class AdminAdapter extends RecyclerView.Adapter<AdminAdapter.ViewHolder>{
 
     List<Admin> adminList=new ArrayList<>();
     Context context;
+
+    private View.OnClickListener OnClick;
 
     public AdminAdapter(List<Admin> adminList, Context context) {
         this.adminList = adminList;
@@ -37,6 +40,11 @@ public class AdminAdapter extends RecyclerView.Adapter<AdminAdapter.ViewHolder>{
         holder.txt_email.setText(""+adminList.get(position).getEmail());
     }
 
+    public void setOnClickListner(View.OnClickListener onClickListner)
+    {
+        OnClick = onClickListner;
+    }
+
     @Override
     public int getItemCount() {
         return adminList.size();
@@ -45,11 +53,14 @@ public class AdminAdapter extends RecyclerView.Adapter<AdminAdapter.ViewHolder>{
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView txt_email;
-
+        ImageView img_delete;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txt_email=itemView.findViewById(R.id.txt_adminEmail);
+            img_delete=itemView.findViewById(R.id.img_delete);
 
+            img_delete.setTag(this);
+            img_delete.setOnClickListener(OnClick);
         }
     }
 }
--- app/src/main/java/com/example/remindmeat/View/AdminActivity.java
+++ app/src/main/java/com/example/remindmeat/View/AdminActivity.java
@@ -7,8 +7,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.remindmeat.Adapter.AdminAdapter;
 import com.example.remindmeat.Model.Admin;
 import com.example.remindmeat.R;
@@ -89,8 +97,56 @@ public class AdminActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adminAdapter=new AdminAdapter(adminList,AdminActivity.this);
         recyclerView.setAdapter(adminAdapter);
-
+        adminAdapter.setOnClickListner(adapterClick);
     }
 
+    View.OnClickListener adapterClick=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.img_delete:
+                    deleteUser(view);
+                    break;
+            }
+        }
+    };
 
+    private void deleteUser(View view) {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        RecyclerView.ViewHolder viewHolder=(RecyclerView.ViewHolder) view.getTag();
+        final int position = viewHolder.getAdapterPosition();
+        String url ="http://10.123.157.102:3000/delete/"+adminList.get(position).getUId();
+        Log.d("AdminActivity","Link"+url);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        db.collection("Users").document(adminList.get(position).getUId()).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(AdminActivity.this, "Deleted successfully", Toast.LENGTH_SHORT).show();
+                                    adminList.remove(position);
+                                    adminAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        });
+
+
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("AdminActivity","Error="+error.getMessage());
+                Toast.makeText(AdminActivity.this, "Didn't work"+error.getMessage(), Toast.LENGTH_SHORT).show();
+                // textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
 }
